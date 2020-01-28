79028bde97541789a362110425c36232f9bfa9ff
ilya <ilya@2cit.ru>
2020-01-28 23:10:17 +0300

    fix provision script

--- provision.sh
+++ provision.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 ./inventory.sh > ansible/hosts
+
+echo "[faceless-vpn-server-ike]" > ansible/host_${1}
+cat vpn_server_${1}.txt >> ansible/host_${1}
+echo "[all:vars]" >> ansible/host_${1}
+echo "project=${ANSIBLE_PROJECT}" >> ansible/host_${1}
+
 cd ansible
-ansible-playbook -i hosts playbooks/server.yml
+ansible-playbook -i host_${1} playbooks/server.yml
