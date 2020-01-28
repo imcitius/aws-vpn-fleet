28d8e35d8e1969239b27ac989f73150eb8ddc9e1
ilya <ilya@2cit.ru>
2020-01-28 19:14:50 +0300

    updates

--- provision.sh
+++ provision.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd vpn
-ansible-playbook -i `cat ../private_ip.txt` --ssh-user centos --site-config ../inventory.yml
+#cd ansible
+#ansible-playbook -i `cat ../private_ip.txt` --ssh-user centos --site-config ../inventory.yml
