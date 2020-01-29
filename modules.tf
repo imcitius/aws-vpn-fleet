a8b078313f1cdefb98219385c11eb54458bb413e
ilya <ilya@2cit.ru>
2020-01-29 15:31:53 +0300

    update var name

--- modules.tf
+++ modules.tf
@@ -4,7 +4,8 @@ module "vpn-server" {
   servers_count = var.servers_count
   aws_region = var.aws_region
   ssh_key_name = var.ssh_key_name
-  dns_domain = var.dns_domain
+  dns_vpn_subdomain = var.dns_vpn_subdomain
+  dns_global_domain = var.dns_global_domain
   cloudflare_zone_id = var.cloudflare_zone_id
   vpn_ami_id = var.vpn_ami_id
   vpn_instance_type = var.vpn_instance_type
--- variables.tf
+++ variables.tf
@@ -3,7 +3,8 @@ variable "ssh_key_name" {}
 variable "servers_count" {
     default = "1"
 }
-variable "dns_domain" {}
+variable "dns_vpn_subdomain" {}
+variable "dns_global_domain" {}
 variable "cloudflare_api_token" {}
 variable "cloudflare_zone_id" {}
 variable "vpn_ami_id" {}
