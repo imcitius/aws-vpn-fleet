c906efe3a3718134d127b0c83ef3c99030440336
ilya <ilya@2cit.ru>
2020-01-30 23:53:14 +0300

    multi-region

--- modules.tf
+++ modules.tf
@@ -1,16 +1,42 @@
 module "vpn-server" {
   source  = "git@jgit.me:infrastructure/terraform/aws-vpn-server.module.git?ref=centos7"
 
-  servers_count = var.servers_count
-  aws_region = var.aws_region
-  ssh_key_name = var.ssh_key_name
-  dns_vpn_subdomain = var.dns_vpn_subdomain
-  dns_global_domain = var.dns_global_domain
-  cloudflare_zone_id = var.cloudflare_zone_id
-  vpn_ami_id = var.vpn_ami_id
-  vpn_instance_type = var.vpn_instance_type
-  ansible_project = var.ansible_project
-  aws_sns_topic = var.aws_sns_topic
-  tg_chat_id = var.tg_chat_id
-  tg_token = var.tg_token
+  servers_count = var.frankfurt["servers_count"]
+  aws_region = var.frankfurt["aws_region"]
+  ssh_key_name = var.frankfurt["ssh_key_name"]
+  dns_vpn_subdomain = var.frankfurt["dns_vpn_subdomain"]
+  dns_global_domain = var.frankfurt["dns_global_domain"]
+  cloudflare_zone_id = var.frankfurt["cloudflare_zone_id"]
+  vpn_ami_id = var.frankfurt["vpn_ami_id"]
+  vpn_instance_type = var.frankfurt["vpn_instance_type"]
+  ansible_project = var.frankfurt["ansible_project"]
+  tg_chat_id = var.frankfurt["tg_chat_id"]
+  tg_token = var.frankfurt["tg_token"]
+
+  aws_iam_role_iam_for_lambda = aws_iam_role.iam_for_lambda.arn
+
+}
+
+module "vpn-server-korea" {
+  source  = "git@jgit.me:infrastructure/terraform/aws-vpn-server.module.git?ref=centos7"
+
+  providers = {
+    aws = aws.ap-northeast-2
+    cloudflare = cloudflare.ap-northeast-2
+  }
+
+  servers_count = var.seoul["servers_count"]
+  aws_region = var.seoul["aws_region"]
+  ssh_key_name = var.seoul["ssh_key_name"]
+  dns_vpn_subdomain = var.seoul["dns_vpn_subdomain"]
+  dns_global_domain = var.seoul["dns_global_domain"]
+  cloudflare_zone_id = var.seoul["cloudflare_zone_id"]
+  vpn_ami_id = var.seoul["vpn_ami_id"]
+  vpn_instance_type = var.seoul["vpn_instance_type"]
+  ansible_project = var.seoul["ansible_project"]
+  tg_chat_id = var.seoul["tg_chat_id"]
+  tg_token = var.seoul["tg_token"]
+
+  aws_iam_role_iam_for_lambda = aws_iam_role.iam_for_lambda.arn
+
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,18 @@
-variable "aws_region" {}
-variable "ssh_key_name" {}
-variable "servers_count" {
-    default = "1"
-}
-variable "dns_vpn_subdomain" {}
-variable "dns_global_domain" {}
-variable "cloudflare_api_token" {}
-variable "cloudflare_zone_id" {}
-variable "vpn_ami_id" {}
-variable "vpn_instance_type" {}
-variable "ansible_project" {}
-variable "aws_sns_topic" {}
-variable "tg_chat_id" {}
-variable "tg_token" {}
+// variable "aws_region" {}
+// variable "ssh_key_name" {}
+// variable "servers_count" {
+//     default = "1"
+// }
+// variable "dns_vpn_subdomain" {}
+// variable "dns_global_domain" {}
+// variable "cloudflare_api_token" {}
+// variable "cloudflare_zone_id" {}
+// variable "vpn_ami_id" {}
+// variable "vpn_instance_type" {}
+// variable "ansible_project" {}
+// variable "aws_sns_topic" {}
+// variable "tg_chat_id" {}
+// variable "tg_token" {}
+
+variable "frankfurt" {}
+variable "seoul" {}
