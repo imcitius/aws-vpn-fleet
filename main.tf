5b12970a781f9a28bbb0db29a079c676a8327cd9
Ilya Rubinchik <ilya@2cit.ru>
2020-02-18 15:24:18 +0300

    updates

--- main.tf
+++ main.tf
@@ -1,17 +1,14 @@
-// terraform {
-//   backend "consul" {
-//     address = "consul.service.infra1.consul:8500"
-//     scheme  = "http"
-//     path    = "tf/states/faceless-vpn-server-eucentral1"
-//   }
-// }
+//terraform {
+//  backend "local" {  }
+//}
 
 terraform {
   backend "http" {
     address = "http://terraform-state-store.service.faceless-prod.consul/v1/state/main"
     lock_address = "http://terraform-state-store.service.faceless-prod.consul/v1/state/main"
     unlock_address = "http://terraform-state-store.service.faceless-prod.consul/v1/state/main"
-    username = var.ansible_project
+    username = "faceless"
     password = "any"
   }
 }
+
